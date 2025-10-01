f513dbae2f2cf565290e37aa506534c9fab475ca
gerly <gherly.ylrehg@gmail.com>
2025-10-01 07:09:34 +0700

    Import file text_widget.dart ke main.dart

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 A new Flutter project.
 
 ![Screenshot hello_world](images/01.png)
+
+Import file text_widget.dart ke main.dart
+
+![Screenshot hello_world](images/02.png)
---
+++ lib/basic_widgets/text_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyTextWidget extends StatelessWidget {
+  const MyTextWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      "Nama saya Gerly Vaeyungfan, sedang belajar Pemrograman Mobile",
+      style: TextStyle(
+        color: Colors.red,
+        fontSize: 14,
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
+}
